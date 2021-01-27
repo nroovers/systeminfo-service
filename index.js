b66b1f2c9ea437eb72b0c8ef888b86affa35a772
Nicolai Roovers <nroovers@gmail.com>
2021-01-27 18:39:35 +0200

    added log message

--- index.js
+++ index.js
@@ -6,5 +6,6 @@ const server = http.createServer(app)
 const PORT = process.env.PORT || 3001
 server.listen(PORT, () => {
     //   logger.info(`Server running on port ${config.PORT}`)
+    console.log(`Node_ENV:${process.env.NODE_ENV}.`)
     console.log(`Server running on port ${PORT}`)
 })
