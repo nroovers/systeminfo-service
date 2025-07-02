219656adf6b44cb7443d0f3e1a7361a11c1e7538
nicolai-twoday <nicolai.roovers@twoday.com>
2025-07-02 14:48:53 +0300

    update gitignore

--- dist/routes/infoRoutes.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var express_1 = require("express");
-var infoController_1 = require("controllers/infoController");
-var infoRouter = (0, express_1.Router)();
-infoRouter.get("/", infoController_1.getInfo);
-exports.default = infoRouter;
